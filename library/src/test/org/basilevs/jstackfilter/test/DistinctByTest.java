66de966a93053992e78a0e0a5ba59e14252f35af
Vasili Gulevich <vasili.gulevich@xored.com>
2023-02-27 13:51:00 +0400

    Test for distinct combine in DistinctBy

--- library/src/test/org/basilevs/jstackfilter/test/DistinctByTest.java
+++ library/src/test/org/basilevs/jstackfilter/test/DistinctByTest.java
@@ -2,6 +2,7 @@ package org.basilevs.jstackfilter.test;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import org.basilevs.jstackfilter.DistinctBy;
@@ -11,11 +12,19 @@ import org.junit.Test;
 public class DistinctByTest {
 
 	@Test
-	public void testCombine() {
+	public void combineSame() {
 		var result = Stream.generate(() -> Integer.valueOf(1))
 				.parallel()
 				.limit(10000).collect(new DistinctBy<>(Objects::equals));
 		Assert.assertEquals("No multiple entries of same value", List.of(1), result);
+	}
+
+	@Test
+	public void combineDistinct() {
+		var result = IntStream.range(0, 10000).mapToObj(Integer::valueOf)
+				.parallel()
+				.limit(10000).collect(new DistinctBy<>(Objects::equals));
+		Assert.assertEquals(10000, result.size());
 	}
 
 }
