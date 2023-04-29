948c3ff1adcf8d715a7792ea29f96fc03cc4431f
Vasili Gulevich <basilevs@gmail.com>
2023-04-29 12:34:37 +0400

    Remove stale imports

--- ui/src/org/basilevs/jstackfilter/ui/internal/SystemUtil.java
+++ ui/src/org/basilevs/jstackfilter/ui/internal/SystemUtil.java
@@ -1,7 +1,5 @@
 package org.basilevs.jstackfilter.ui.internal;
 
-import java.io.Closeable;
-import java.io.FilterInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
