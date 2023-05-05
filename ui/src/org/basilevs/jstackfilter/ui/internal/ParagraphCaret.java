b275a1b0528ba9c2eb39272d48c90f2ab1d39c82
Vasili Gulevich <basilevs@gmail.com>
2023-05-05 17:24:20 +0400

    Minor refactoring

--- ui/src/org/basilevs/jstackfilter/ui/internal/ParagraphCaret.java
+++ ui/src/org/basilevs/jstackfilter/ui/internal/ParagraphCaret.java
@@ -5,6 +5,7 @@ import java.lang.System.Logger;
 import java.lang.System.Logger.Level;
 
 import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.text.DefaultCaret;
 import javax.swing.text.JTextComponent;
 import javax.swing.text.Position.Bias;
@@ -15,60 +16,61 @@ public class ParagraphCaret extends DefaultCaret {
 	private static final long serialVersionUID = -3113229730163357205L;
 	private static final Logger LOG = System.getLogger(ParagraphCaret.class.getName());
 
+	private abstract class RangeChangeAction extends AbstractAction {
+		private static final long serialVersionUID = 3515558612059027621L;
+
+		public RangeChangeAction(String name) {
+			super(name);
+		}
+
+		@Override
+		public final void actionPerformed(ActionEvent e) {
+			Range range = newRange(getComponent().getText(), getMark(), getDot());
+			LOG.log(Level.DEBUG, getValue(Action.NAME) + ": {0} {1} {2}", getMark(), getDot(), range);
+			ParagraphCaret.super.setDot(range.from, Bias.Forward);
+			ParagraphCaret.super.moveDot(range.to, Bias.Forward);
+
+		}
+
+		protected abstract Range newRange(String text, int mark, int dot);
+	}
+
 	@Override
 	public void install(JTextComponent c) {
 		super.install(c);
-		c.getActionMap().put("caret-up", new AbstractAction("caret-up") {
+		c.getActionMap().put("caret-up", new RangeChangeAction("caret-up") {
 			private static final long serialVersionUID = 3515558612059027621L;
 
 			@Override
-			public void actionPerformed(ActionEvent e) {
-				Range range = Paragraphs.findPreviousParagraphRange(getComponent().getText(), getMark(), getDot());
-				LOG.log(Level.DEBUG, "caret-up: {0} {1} {2}", getMark(), getDot(), range);
-				ParagraphCaret.super.setDot(range.from, Bias.Forward);
-				ParagraphCaret.super.moveDot(range.to, Bias.Forward);
-				
+			public Range newRange(String text, int mark, int dot) {
+				return Paragraphs.findPreviousParagraphRange(text, mark, dot);
 			}
 		});
 
-		c.getActionMap().put("caret-down", new AbstractAction("caret-down") {
-
+		c.getActionMap().put("caret-down", new RangeChangeAction("caret-down") {
 			private static final long serialVersionUID = 6732567272487548873L;
-
 			@Override
-			public void actionPerformed(ActionEvent e) {
-				Range range = Paragraphs.findNextParagraphRange(getComponent().getText(), getMark(), getDot());
-				LOG.log(Level.DEBUG, "caret-down: {0} {1} {2}", getMark(), getDot(), range);
-				ParagraphCaret.super.setDot(range.from, Bias.Forward);
-				ParagraphCaret.super.moveDot(range.to, Bias.Forward);
+			public Range newRange(String text, int mark, int dot) {
+				return Paragraphs.findNextParagraphRange(text, mark, dot);
 			}
 		});
 
-		c.getActionMap().put("selection-up", new AbstractAction("selection-up") {
-
+		c.getActionMap().put("selection-up", new RangeChangeAction("selection-up") {
 			private static final long serialVersionUID = 6004582118340973608L;
-
 			@Override
-			public void actionPerformed(ActionEvent e) {
-				Range range = Paragraphs.includePreviousParagraph(getComponent().getText(), getMark(), getDot());
-				LOG.log(Level.DEBUG, "selection-up: {0} {1} {2}", getMark(), getDot(), range);
-				ParagraphCaret.super.setDot(range.from, Bias.Forward);
-				ParagraphCaret.super.moveDot(range.to, Bias.Forward);
+			public Range newRange(String text, int mark, int dot) {
+				return Paragraphs.includePreviousParagraph(text, mark, dot);
 			}
 		});
-		
-		c.getActionMap().put("selection-down", new AbstractAction("selection-down") {
+
+		c.getActionMap().put("selection-down", new RangeChangeAction("selection-down") {
 			private static final long serialVersionUID = 9093329936068012782L;
-
 			@Override
-			public void actionPerformed(ActionEvent e) {
-				Range range = Paragraphs.includeNextParagraph(getComponent().getText(), getMark(), getDot());
-				LOG.log(Level.DEBUG, "selection-down: {0} {1} {2}", getMark(), getDot(), range);
-				ParagraphCaret.super.setDot(range.from, Bias.Forward);
-				ParagraphCaret.super.moveDot(range.to, Bias.Forward);
+			public Range newRange(String text, int mark, int dot) {
+				return Paragraphs.includeNextParagraph(text, mark, dot);
 			}
 		});
-		
+
 	}
 
 	@Override
