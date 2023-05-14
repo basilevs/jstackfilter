da5701a51d26a0731fece02c58229b5891442849
Vasili Gulevich <basilevs@gmail.com>
2023-05-14 22:57:05 +0400

    Ignore case when detecting OS

--- library/src/main/java/org/basilevs/jstackfilter/internal/OS.java
+++ library/src/main/java/org/basilevs/jstackfilter/internal/OS.java
@@ -3,43 +3,40 @@ package org.basilevs.jstackfilter.internal;
 import java.io.FileNotFoundException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public enum OS {
 	MACOS {
 		@Override
 		public Path configurationDirectory() throws FileNotFoundException {
-			return checkWritable(Path.of(System.getProperty("user.home"), "Library", "Application Support", "org.basilevs.jstackfilter"));
+			return ensureWritable(Paths.get(System.getProperty("user.home"), "Library", "Application Support", "org.basilevs.jstackfilter"));
 		}
 	},
 	WINDOWS {
 		@Override
 		public Path configurationDirectory() throws FileNotFoundException {
-			return checkWritable(Path.of(System.getenv("APPDATA"), "basilevs", "jstackfilter"));
+			return ensureWritable(Paths.get(System.getenv("APPDATA"), "basilevs", "jstackfilter"));
 		}
 	},
 	LINUX {
 		@Override
 		public Path configurationDirectory() throws FileNotFoundException {
-			return checkWritable(Path.of(System.getProperty("user.home"), ".config", "jstackfilter"));
+			return ensureWritable(Paths.get(System.getProperty("user.home"), ".config", "jstackfilter"));
 		}
 	};
 
-	public static OS parseOsName(String name) {
-		if (name.startsWith("Mac OS X")) {
+	public static OS detect() {
+		String osName = System.getProperty("os.name").toLowerCase();
+		if (osName.contains("mac")) {
 			return MACOS;
-		} else if (name.startsWith("Windows")) {
+		} else if (osName.contains("win")) {
 			return WINDOWS;
-		} else if (name.startsWith("Linux")) {
+		} else if (osName.contains("nix") || osName.contains("nux")) {
 			return LINUX;
 		} else {
-			throw new IllegalArgumentException("Unknown OS: " + name);
+			throw new IllegalArgumentException("Unsupported OS: " + osName);
 		}
 	}
-
-	public static OS detect() {
-		return parseOsName(System.getProperty("os.name"));
-	}
-
 	public abstract Path configurationDirectory() throws FileNotFoundException; 
 	
 	private static Path getExistingParent(Path path) {
@@ -51,10 +48,10 @@ public enum OS {
 		throw new AssertionError("No parent for " + path);
 	}
 	
-	private static Path checkWritable(Path file) throws FileNotFoundException {
+	private static Path ensureWritable(Path file) throws FileNotFoundException {
 		Path parent = getExistingParent(file);
 		if (! Files.isDirectory(parent) || ! Files.isWritable(parent)) {
-			throw new FileNotFoundException("Cannot write to " + file);
+			throw new FileNotFoundException("Cannot write to " + file + " because " + parent + " is not writable");
 		}
 		return file;
 		
