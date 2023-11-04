051afea21d379d6a49a5efa340cbc84ab105921c
Vasili Gulevich <basilevs@gmail.com>
2023-11-04 05:03:02 +0400

    Support view recreation
    New views are synchronized with the command for consistency

--- eclipse/plugin/src/org/basilevs/jstackfilter/eclipse/ShowIdleThreadsHandler.java
+++ eclipse/plugin/src/org/basilevs/jstackfilter/eclipse/ShowIdleThreadsHandler.java
@@ -1,45 +1,30 @@
 package org.basilevs.jstackfilter.eclipse;
 
-import java.util.Arrays;
-import java.util.function.Predicate;
+import java.util.Collections;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.debug.ui.IDebugView;
-import org.eclipse.jface.viewers.StructuredViewer;
-import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 public class ShowIdleThreadsHandler extends AbstractHandler {
-	
-	private final IdleThreadFilter filter = new IdleThreadFilter();
-	
+
 	@Override
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		boolean checked = HandlerUtil.toggleCommandState(event.getCommand());
-		StructuredViewer viewer = getStructuredViewer(event);
-		if (checked) {
-			if (!hasFilter(viewer)) { 
-				viewer.addFilter(filter);
-			}
-		} else {
-			viewer.removeFilter(filter);
+	public void setEnabled(Object evaluationContext) {
+		super.setEnabled(evaluationContext);
+		var event = new ExecutionEvent(null, Collections.emptyMap(), null, evaluationContext);
+		try {
+			PartManager.updateCommandFromPart(HandlerUtil.getActivePartChecked(event));
+		} catch (ExecutionException e) {
+			throw new IllegalStateException(e);
 		}
-		return null;
 	}
 
-	private boolean hasFilter(StructuredViewer viewer) {
-		return Arrays.stream(viewer.getFilters()).anyMatch(Predicate.isEqual(filter));
-	}
-	
-	protected StructuredViewer getStructuredViewer(ExecutionEvent event) {
-		IDebugView view = HandlerUtil.getActivePart(event).getAdapter(IDebugView.class);
-		Viewer viewer = view.getViewer();
-		if (viewer instanceof StructuredViewer) {
-			return (StructuredViewer)viewer;
-		}
-		throw new IllegalArgumentException("Is not called from debug view");
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		HandlerUtil.toggleCommandState(event.getCommand());
+		PartManager.updatePartFromCommand(HandlerUtil.getActivePartChecked(event));
+		return null;
 	}
 
 }
