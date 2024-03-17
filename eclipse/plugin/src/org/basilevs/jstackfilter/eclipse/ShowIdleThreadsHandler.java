6db60676c111f17ba4cc0c55a080df4db4748d69
Vasili Gulevich <basilevs@gmail.com>
2024-03-17 23:45:20 +0400

    Use fixed AbstractHandlerWithState.

--- eclipse/plugin/src/org/basilevs/jstackfilter/eclipse/ShowIdleThreadsHandler.java
+++ eclipse/plugin/src/org/basilevs/jstackfilter/eclipse/ShowIdleThreadsHandler.java
@@ -3,22 +3,13 @@ package org.basilevs.jstackfilter.eclipse;
 import java.util.Collections;
 
 import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.AbstractHandlerWithState;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.State;
 import org.eclipse.ui.handlers.HandlerUtil;
 
-public class ShowIdleThreadsHandler extends AbstractHandler {
-
-	@Override
-	public void setEnabled(Object evaluationContext) {
-		super.setEnabled(evaluationContext);
-		var event = new ExecutionEvent(null, Collections.emptyMap(), null, evaluationContext);
-		try {
-			PartManager.updateCommandFromPart(HandlerUtil.getActivePartChecked(event));
-		} catch (ExecutionException e) {
-			throw new IllegalStateException(e);
-		}
-	}
+public class ShowIdleThreadsHandler extends AbstractHandlerWithState {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
@@ -29,6 +20,10 @@ public class ShowIdleThreadsHandler extends AbstractHandler {
 	
 	public static ExecutionEvent adapt(Object evaluationContext) {
 		return new ExecutionEvent(null, Collections.emptyMap(), null, evaluationContext);
+	}
+
+	@Override
+	public void handleStateChange(State state, Object oldValue) {		
 	}
 
 
