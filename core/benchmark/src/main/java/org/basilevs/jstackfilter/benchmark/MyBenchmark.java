c267d9266c74833e93a0ebf1b1c8e9983faa11d6
Vasili Gulevich <basilevs@gmail.com>
2025-07-21 00:17:09 +0400

    Parser benchmark

---
+++ core/benchmark/src/main/java/org/basilevs/jstackfilter/benchmark/MyBenchmark.java
@@ -0,0 +1,32 @@
+package org.basilevs.jstackfilter.benchmark;
+
+import java.io.StringReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Scanner;
+
+import org.basilevs.jstackfilter.JavaThread;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+
+@State(Scope.Benchmark)
+public class MyBenchmark {
+	public String input;
+
+	@Setup
+	public void setup() {
+		try (Scanner scanner = new Scanner(getClass().getResourceAsStream("/eclipse.txt"), StandardCharsets.UTF_8)) {
+			input = scanner.useDelimiter("\\A").next();
+		}
+	}
+	
+    @Benchmark
+    public List<JavaThread> parse() {
+    	try (StringReader stringReader = new StringReader(input)) {
+			return org.basilevs.jstackfilter.JstackParser.parseThreads(stringReader).toList();
+		}
+    }
+
+}
